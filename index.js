8e97de4ec8b381c8ccb03f0bb5370e6d136b4ce4
Marufa Akter <marufaakter1947@gmail.com>
2025-11-09 17:47:02 +0600

    Creating Get API for single Food item

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -36,6 +36,15 @@ async function run() {
     app.get("/all-foods", async (req,res)=>{
 
         const result = await foodCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.get("/all-foods/:id", async (req,res)=>{
+        const {id} = req.params
+        // console.log(id)
+        const objectId = new ObjectId(id)
+        const result = await foodCollection.findOne({_id: objectId})
+
         res.send(result)
     })
 
