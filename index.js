c4c667ddda9b46e97eff2ec50537b4859a45373f
Marufa Akter <marufaakter1947@gmail.com>
2025-11-09 12:17:46 +0600

    Manually add data in Mongodb and creating GET API for all foods

--- index.js
+++ index.js
@@ -27,6 +27,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const db = client.db("food-db")
+    const foodCollection = db.collection("foods")
+
+    // find
+    app.get("/foods", async (req,res)=>{
+
+        const result = await foodCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
